a7a043a8d9c5945cd02b11ba5f0a449f20e7f2d4
Oriol Closa <oriol.closa@est.fib.upc.edu>
2020-04-10 23:53:23 +0200

    Allow send welcome email from admin

--- user/admin.py
+++ user/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 
 from user.models import User
+from user.utils import send_imported
+
+
+def send_welcome(modeladmin, request, users):
+    for user in users:
+        send_imported(user=user)
+
+
+send_welcome.short_description = "Send welcome email"
 
 
 @admin.register(User)
@@ -9,3 +18,4 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ("email", "name", "surname", "type")
     list_filter = ("type", "email_verified", "is_active")
     ordering = ("name", "surname", "email", "type")
+    actions = [send_welcome]

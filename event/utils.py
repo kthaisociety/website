c639eec75c9256220f314c2f72ff62d7c34f4a67
Oriol Closa <oriol.closa@est.fib.upc.edu>
2020-04-21 18:08:30 +0200

    Fix duplicated events

--- event/utils.py
+++ event/utils.py
@@ -4,8 +4,8 @@ from event.models import Event
 
 
 def get_future_events() -> List[Event]:
-    return Event.objects.published_future().order_by("sessions__starts_at")[:3]
+    return list(set(Event.objects.published_future().order_by("sessions__starts_at")))[:3]
 
 
 def get_past_events() -> List[Event]:
-    return Event.objects.published_past().order_by("-sessions__starts_at")[:3]
+    return list(set(Event.objects.published_past().order_by("-sessions__starts_at")))[:3]

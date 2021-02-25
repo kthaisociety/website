b18267eb70b72bb481e84b170b30da076e31d973
Oriol Closa <oriol.closa@est.fib.upc.edu>
2021-02-26 00:15:23 +0100

    Refactor board

--- user/admin.py
+++ user/admin.py
@@ -29,7 +29,7 @@ class UserAdmin(admin.ModelAdmin):
     search_fields = ("id", "email", "name", "surname", "type")
     list_display = ("email", "name", "surname", "type", "created_at")
     list_filter = ("type", "email_verified", "is_active")
-    ordering = ("name", "surname", "email", "type")
+    ordering = ("-created_at",)
     actions = [send_welcome, send_slack_invite]
 
 

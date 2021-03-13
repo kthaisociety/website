3f614578a7775743c8a8515dd28762745631d27d
Oriol Closa <oriol.closa@est.fib.upc.edu>
2021-03-13 19:26:33 +0100

    Add bot reaction and reply

--- messaging/api/views.py
+++ messaging/api/views.py
@@ -1,6 +1,6 @@
 import json
 
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
@@ -14,6 +14,9 @@ def slack_event(request):
     event_type = body.get("type")
     if event_type == "event_callback":
         run(body=body.get("event"))
-    return JsonResponse(
-        {"challenge": body.get("challenge")}
-    )
+    challenge = body.get("challenge")
+    if challenge:
+        return JsonResponse(
+            {"challenge": body.get("challenge")}
+        )
+    return HttpResponse()

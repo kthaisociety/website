6da4c16601733bbe54307f5c1a9afcf11949180c
Oriol Closa <oriol.closa@est.fib.upc.edu>
2021-03-13 19:15:33 +0100

    Add bot reaction and reply

---
+++ messaging/api/slack/reaction.py
@@ -0,0 +1,14 @@
+import slack
+
+from app.enums import SlackError
+from app.settings import SL_TOKEN, SL_CHANNEL_WEBDEV
+from app.slack import send_error_message
+
+
+def add(channel_id: str, message_id: str, emoji: str) -> bool:
+    if SL_TOKEN and SL_CHANNEL_WEBDEV:
+        client = slack.WebClient(SL_TOKEN)
+        response = client.reactions_add(channel=channel_id, timestamp=message_id, name=emoji)
+        if not response.status_code == 200 or not response.data.get("ok", False):
+            return send_error_message(error=SlackError.ADD_REACTION)
+        return True
--- messaging/api/views.py
+++ messaging/api/views.py
@@ -4,11 +4,14 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 
+from messaging.api.slack.event import run
+
 
 @csrf_exempt
 @require_http_methods(["POST"])
 def slack_event(request):
     body = json.loads(request.body)
+    run(body=body)
     return JsonResponse(
         {"challenge": body.get("challenge")}
     )

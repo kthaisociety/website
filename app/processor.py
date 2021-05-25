31fcda5782ed6f87d68fca4ede2c8116ac45638f
Oriol Closa <oriol.closa@est.fib.upc.edu>
2021-05-25 18:55:35 +0200

    Recover events on sidebar

--- app/processor.py
+++ app/processor.py
@@ -4,7 +4,7 @@ from app.utils import get_substitutions_templates
 def variables_processor(request=None):
     c = get_substitutions_templates(request=request)
     from news.utils import get_latest_articles
-    from event.utils import get_future_events
+    from event.utils import get_future_events, get_events
     from user.utils import get_organisers, get_board, get_histories
     from page.utils import get_menu_pages
     from business.utils import get_sponsorships, get_offers
@@ -15,6 +15,8 @@ def variables_processor(request=None):
 
     c["articles"] = get_latest_articles()
     c["events"] = get_future_events()
+    if not c["events"]:
+        c["events"] = get_events()
     c["organisers"] = get_organisers()
     c["board"] = get_board()
     c["categories"] = get_menu_pages()

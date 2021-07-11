0a653c2ca68c5e404e9216c6085c0b56d647eb1d
Oriol Closa <oriol.closa@est.fib.upc.edu>
2021-07-11 21:22:57 +0200

    Add featured

--- news/utils.py
+++ news/utils.py
@@ -4,4 +4,18 @@ from news.models import Article
 
 
 def get_latest_articles() -> List[Article]:
-    return Article.objects.published().order_by("-created_at")[:5]
+    featured_article = get_featured_article()
+    return (
+        Article.objects.published()
+        .exclude(id=featured_article.id)
+        .order_by("-created_at")[:5]
+    )
+
+
+def get_featured_article() -> Article:
+    return (
+        Article.objects.published()
+        .filter(is_featured=True)
+        .order_by("-created_at")
+        .first()
+    )

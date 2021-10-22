7238a060b146a6c8de2c1418a972f1e3c7f9260d
Oriol Closa <oriolcm@kth.se>
2021-10-22 19:19:12 +0200

    Publish job offers only once

--- business/cron.py
+++ business/cron.py
@@ -7,7 +7,7 @@ from messaging.api.slack.announcement import announce_job_offers
 def announce_latest_job_offers():
     job_offers = Offer.objects.filter(
         is_visible=True,
-        starts_at__gte=timezone.now() - timezone.timedelta(days=7),
+        starts_at__gte=timezone.now() - timezone.timedelta(days=1),
         starts_at__lt=timezone.now(),
     )
     if job_offers:

f5ac1e7c8584f5dd1b8edd82ede41dc98377470b
Oriol Closa <oriolcm@kth.se>
2021-12-04 18:49:02 +0100

    Add QR for late registration

--- event/urls.py
+++ event/urls.py
@@ -5,6 +5,11 @@ from event import views
 urlpatterns = [
     url(r"^event/ics/(?P<code>.*).ics$", views.event_ics, name="events_event_ics"),
     url(r"^event/(?P<code>.*)/live$", views.live, name="events_live"),
+    url(
+        r"^event/(?P<event_id>.*)/register$",
+        views.checkin_event_register,
+        name="events_checkin_event_register",
+    ),
     url(r"^event/(?P<code>.*)$", views.event, name="events_event"),
     url(
         r"^registration/(?P<registration_id>.*)/url$",
@@ -17,6 +22,11 @@ urlpatterns = [
         r"^checkin/attend/(?P<registration_id>.*)$",
         views.checkin_event_attend,
         name="events_checkin_event_attend",
+    ),
+    url(
+        r"^checkin/(?P<code>.*)/qr$",
+        views.checkin_event_qr,
+        name="events_checkin_event_qr",
     ),
     url(r"^checkin/(?P<code>.*)$", views.checkin_event, name="events_checkin_event"),
 ]

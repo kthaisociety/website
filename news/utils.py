bfa3206aedd6530627406174fe76b3c755348a9c
Oriol Closa <oriolcm@kth.se>
2022-04-16 15:00:56 +0200

    Add pins to deprecate featured news

--- news/utils.py
+++ news/utils.py
@@ -1,22 +1,11 @@
 from typing import List
 
-from news.models import Article
+from news.models import Article, Pin
 
 
 def get_latest_articles() -> List[Article]:
-    articles = Article.objects.published()
-
-    featured_article = get_featured_article()
-    if featured_article:
-        articles.exclude(id=featured_article.id)
-
-    return articles.order_by("-created_at")[:5]
+    return Article.objects.published().order_by("-created_at")[:5]
 
 
-def get_featured_article() -> Article:
-    return (
-        Article.objects.published()
-        .filter(is_featured=True)
-        .order_by("-created_at")
-        .first()
-    )
+def get_latest_pin() -> Pin:
+    return Pin.objects.order_by("-created_at").first()

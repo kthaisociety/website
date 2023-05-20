8ac0448fa66ca92451ed218f04d3796ed72aab89
Oriol Closa <oriolcm@kth.se>
2023-05-20 22:26:22 +0200

    Allow 4 events for AI week

--- app/processor.py
+++ app/processor.py
@@ -15,9 +15,7 @@ def variables_processor(request=None):
 
     c["pin"] = get_latest_pin()
     c["articles"] = get_latest_articles()
-    c["events"] = get_future_events()
-    if not c["events"]:
-        c["events"] = get_events()
+    c["events"] = get_events()
     c["organisers"] = get_organisers()
     c["board"] = get_board()
     c["categories"] = get_menu_pages()
